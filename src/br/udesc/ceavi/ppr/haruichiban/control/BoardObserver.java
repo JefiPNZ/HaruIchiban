bd5a6ff1ac2daa2d1b1592426467ceb823db257e
jefip <jefip@10.60.94.152>
2019-05-02 21:27:25 -0300

    Desenvolver ajustes para mover o desenho do tabuleiro para a view.

---
+++ src/br/udesc/ceavi/ppr/haruichiban/control/BoardObserver.java
@@ -0,0 +1,51 @@
+package br.udesc.ceavi.ppr.haruichiban.control;
+
+import java.awt.Color;
+
+/**
+ *
+ * @author Jeferson Penz
+ */
+public interface BoardObserver {
+    
+    /**
+     * Limpa uma célula do tabuleiro para permitir desenhos futuros.
+     * @param row
+     * @param col 
+     */
+    public void clearTile(int row, int col);
+    
+    /**
+     * Realiza o desenho de uma vitoria régia no tabuleiro.
+     * @param row
+     * @param col
+     * @param isDark
+     * @param rotation
+     */
+    public void drawLilypad(int row, int col, boolean isDark, float rotation);
+    
+    /**
+     * Desenha um ovo de sapo com a cor informada.
+     * @param row
+     * @param col
+     * @param eggColor 
+     */
+    public void drawEgg(int row, int col, Color eggColor);
+    
+    /**
+     * Desenha uma flor com a cor informada.
+     * @param row
+     * @param col
+     * @param flowerColor 
+     */
+    public void drawFlower(int row, int col, Color flowerColor);
+    
+    /**
+     * Desenha um sapo com a cor informada.
+     * @param row
+     * @param col
+     * @param frogColor 
+     */
+    public void drawFrog(int row, int col, Color frogColor);
+    
+}
